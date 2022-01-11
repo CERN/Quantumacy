20f9c1171f46990ec17dc1929693a4a96d23fb2d
Alberto Di Meglio <alberto.di.meglio@cern.ch>
2022-01-11 23:27:35 +0100

    use spawnSynch instead of execSynch to capture both stdout and stderr

--- quantweb/pages/demos/qkd-client.js
+++ quantweb/pages/demos/qkd-client.js
@@ -1,9 +1,14 @@
+import { stderr } from 'process';
+
 export default function QKDClient({ data }) {
   // Render data...
   var newData = "<p>";
   for (let line of data.split('\n')) {
     newData = newData + line + "<br>";
   };
+  if (data == "") {
+    newData = newData + "ERROR:root:Error while connecting to server: [Errno -2] Name or service not known";
+  }
   newData = newData + "</p>";
 
   return (
@@ -14,19 +19,28 @@ export default function QKDClient({ data }) {
 // This gets called on every request
 export async function getServerSideProps() {
   // exec script
+  const command = "QKDSimkit";
+  const args = ["client", "188.184.195.58:5002", "188.184.195.58:5000"];
+  //const command = "QKDSimkit client -h";
   var data = ""
-  var exec = require('child_process').execSync;
+  var exec = require('child_process').spawnSync;
 
-  var result = ""
   try {
-    result = exec('ls -lsa', { encoding: 'utf-8' });
+    var res = exec(command, args, { encoding: 'utf-8' });
+    if ((res.stdout != null) && (res.stderr != null)) {
+      console.log( "stdout: " + res.stdout);
+      console.log( "stderr: " + res.stderr );
+      data = res.stdout + res.stderr;
+    };
+    if (res.error != undefined) {
+      console.log( "Error: " + res.error );
+      data = res.error;
+    };
   }
   catch(ex) {
-    result = ex.stack;
+    data = ex.stack;
+    console.log( "Error: " + data );
   }
-
-  //console.log( result );
-  data = result;
 
   // Pass data to the page via props
   return { props: { data } }
