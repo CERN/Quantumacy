6d76c4c0b7deacdb7927474d92e9727b15bada89
Gabriele Morello <sgabbo99gmail.com>
2022-04-22 14:55:15 +0200

    updated github link in docs/conf.py

--- QKDSimkit/docs/source/conf.py
+++ QKDSimkit/docs/source/conf.py
@@ -97,7 +97,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -193,4 +193,4 @@ def linkcode_resolve(domain, info):
     if not info['module']:
         return None
     filename = info['module'].replace('.', '/')
-    return "https://github.com/CERN/Quantumacy/tree/main/QKDSimkit/Qiskit/%s.py" % filename
+    return "https://github.com/CERN/Quantumacy/tree/main/QKDSimkit/QKDSimkit/%s.py" % filename
