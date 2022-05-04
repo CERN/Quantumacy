c85996d7f858f28fa4defec99dfadac47766da0c
Gabriele Morello <sgabbo99gmail.com>
2022-05-04 12:51:07 +0200

    new branch to fix sphinx's autodoc on rtd

--- QKDSimkit/docs/source/conf.py
+++ QKDSimkit/docs/source/conf.py
@@ -18,8 +18,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
 import os
 import sys
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../QKDSimkit'))
 
@@ -31,7 +34,7 @@ copyright = '2021, Gabriele Morello'
 author = 'Gabriele Morello'
 
 # The short X.Y version
-version = ''
+version = '0.0.7'
 # The full version, including alpha/beta/rc tags
 release = ''
 
